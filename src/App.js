58e320e8478bc11e9cfd5a4be8109ca6f2eca596
Kevin Stobbs <Kevin.Stobbs94@gmail.com>
2023-03-25 14:59:37 +0000

    tidied homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+
 function App() {
+  const appStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "60vh",
+  };
+
+  const textStyle = {
+    fontSize: "24px",
+    textAlign: "center",
+  };
+
   return (
-    <div className="App">
-      Ever want to watch a movie or tv show but not know where it's available?{" "}
-      <br></br>
-      Google no more!<br></br>Just search for what you want to watch and we'll
-      show you where you can find it!
+    <div className="App" style={appStyle}>
+      <p style={textStyle}>
+        Ever want to watch a movie or tv show but not know where it's available?{" "}
+        <br />
+        Google no more!
+        <br />
+        Just search for what you want to watch and we'll show you where you can
+        find it!
+      </p>
     </div>
   );
 }
