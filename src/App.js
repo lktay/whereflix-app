c822f5ce386b2031813fc7e7ae950f4374b3e300
Kevin Stobbs <Kevin.Stobbs94@gmail.com>
2023-03-25 14:31:12 +0000

    uploading for leoni to pull because her push became corrupted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 function App() {
-  return <div className="App">home</div>;
+  return (
+    <div className="App">
+      Ever want to watch a movie or tv show but not know where it's available?
+      Google no more! Just search for what you want to watch and we'll show you
+      where
+    </div>
+  );
 }
 
 export default App;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
-  return <div>sup</div>;
+  return (
+    <div className="about-container">
+      <p className="about-text">
+        Fullstack app for searching movie/tv show streaming availability - by
+        Kevin Stobbs and Leoni Taylor.
+        <br />
+        <br />
+        Links to the repositories:
+        <br />
+        <a
+          href="https://github.com/lktay/whereflix-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/lktay/whereflix-app
+        </a>
+        <br />
+        <a
+          href="https://github.com/lktay/whereflix-server"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/lktay/whereflix-server
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default About;
